404a6912c5633cdc9b3652b6e4166cf86e2d6b3e
PzaThief <vwjdalsgkv@gmail.com>
2021-02-18 23:06:17 +0900

    change to React

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var app = express();
-app.get("/", function (req, res, next) {
-    res.send("hello typescript express!");
-});
-exports.default = app;
-//# sourceMappingURL=app.js.map
