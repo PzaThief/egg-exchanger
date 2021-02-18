34e3746475cc7e2716f9c419320d46c561d667f5
PzaThief <vwjdalsgkv@gmail.com>
2021-02-19 01:01:18 +0900

    top bar setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div className="nav-left">이것저것 환율 계산기</div>
+        <div className="nav-right">
+          <a
+            href="https://github.com/PzaThief/egg-exchanger"
+            target="_blank"
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            코드 보러가기
+          </a>
+        </div>
+      </div>
+      <div className="exchanger-cover">기준화폐</div>
     </div>
   );
 }
