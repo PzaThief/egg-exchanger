e9c9a3b8ba25629111431cb6caf548db96f62318
PzaThief <vwjdalsgkv@gmail.com>
2021-02-20 11:47:39 +0900

    separate backend and frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,13 +7,7 @@ function App() {
       <div className="black-nav">
         <div className="nav-left">이것저것 환율 계산기</div>
         <div className="nav-right">
-          <a
-            href="https://github.com/PzaThief/egg-exchanger"
-            target="_blank"
-            style={{ color: "inherit", textDecoration: "inherit" }}
-          >
-            코드 보러가기
-          </a>
+          <a {...to_sitecode}>코드 보러가기</a>
         </div>
       </div>
       <div className="exchanger-cover"></div>
@@ -21,5 +15,11 @@ function App() {
     </div>
   );
 }
+
+const to_sitecode = {
+  href: "https://github.com/PzaThief/egg-exchanger",
+  target: "_blank",
+  style: { color: "inherit", textDecoration: "inherit" },
+};
 
 export default App;
